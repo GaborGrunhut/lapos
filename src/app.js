2bda5af1f9d7e7af015a8c9bd2edbfb39c9b71f3
GaborGrunhut <116828888+GaborGrunhut@users.noreply.github.com>
2023-02-07 18:29:05 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+/*
+* File: app.js
+* Author: Grünhut Gábor
+* Copyright: 2023, Grünhut Gábor
+* Group: Szoft I-1/E
+* Date: 2023-02-07
+* Github: https://github.com/GaborGrunhut/
+* Licenc: GNU GPL
+*/
+
